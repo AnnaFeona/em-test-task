0b5f8dcdf52af31ce76a291d700e32d759d783c5
AnnaFeona <glavatskikh.anna@gmail.com>
2023-08-21 19:08:39 +0300

    refactor: move core components to a separate folder

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { NotFoundComponent } from './core/not-found/not-found.component';
+import { NotFoundComponent } from './core/components/not-found/not-found.component';
 import AuthGuard from './core/guards/auth.guard';
 
 const routes: Routes = [
